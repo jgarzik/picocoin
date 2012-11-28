21f001d032b37d7351bf0dd1f0252a6c9d2a0732
Jeff Garzik <jgarzik@exmulti.com>
2012-11-28 02:26:18 -0500

    ccoin/compat.h: further GLIB version muckery

--- include/ccoin/compat.h
+++ include/ccoin/compat.h
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include <glib.h>
 
-#ifndef GLIB_VERSION_2_30
+#if ((GLIB_MAJOR_VERSION == 2) && (GLIB_MINOR_VERSION < 30))
 static inline GPtrArray *
 g_ptr_array_new_full (guint          reserved_size,
                       GDestroyNotify element_free_func)
@@ -21,7 +21,7 @@ g_ptr_array_new_full (guint          reserved_size,
   g_ptr_array_set_free_func (array, element_free_func);
   return array;
 }
-#endif /* !GLIB_VERSION_2_30 */
+#endif /* GLIB_VERSION < 2.30 */
 
 #ifndef HAVE_FDATASYNC
 static inline int fdatasync(int fd)
