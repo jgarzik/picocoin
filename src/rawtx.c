33ca9872e44518497e1671bbae6b6de51b1278ad
Jeff Garzik <jgarzik@bitpay.com>
2014-05-07 16:26:01 -0400

    rawtx: add JSON decoded transaction output option

--- src/rawtx.c
+++ src/rawtx.c
@@ -10,11 +10,13 @@
 #include <argp.h>
 #include <ctype.h>
 #include <glib.h>
+#include <jansson.h>
 #include <ccoin/core.h>
 #include <ccoin/util.h>
 #include <ccoin/hexcode.h>
 #include <ccoin/base58.h>
 #include <ccoin/script.h>
+#include <assert.h>
 
 const char *argp_program_version = PACKAGE_VERSION;
 
@@ -37,14 +39,23 @@ static struct argp_option options[] = {
 	{ "delete-txout", 1021, "INDEX", 0,
 	  "Delete transaction output at INDEX" },
 
+	{ "output-json", 1030, NULL, 0,
+	  "Output transaction as decoded JSON" },
+
+	{ "strict-free", 1040, NULL, 0,
+	  "Free memory on exit (default off; only for valgrind)" },
+
 	{ }
 };
+
+static struct bp_tx tx;
 
 static char *opt_locktime;
 static char *opt_version;
 static char *opt_hexdata;
 static bool opt_blank;
-static struct bp_tx tx;
+static bool opt_decode_json;
+static bool opt_strict_free;
 static GList *opt_txin;
 static GList *opt_del_txin;
 static GList *opt_txout;
@@ -84,6 +95,12 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
 		break;
 	case 1003:			// --blank
 		opt_blank = true;
+		break;
+	case 1030:			// --output-json
+		opt_decode_json = true;
+		break;
+	case 1040:			// --strict-free
+		opt_strict_free = true;
 		break;
 
 	case 1010: {			// --txin=TXID:VOUT
@@ -342,7 +359,145 @@ static void read_data(void)
 	g_string_free(txbuf, TRUE);
 }
 
-static void write_data(void)
+static void bp_json_set_new_int(json_t *obj, const char *key, json_int_t i)
+{
+	json_t *val = json_integer(i);
+	assert(val != NULL);
+
+	int rc = json_object_set_new(obj, key, val);
+	assert(rc == 0);
+}
+
+static void bp_json_set_new_int256(json_t *obj, const char *key, const bu256_t *i)
+{
+	char hexstr[(32 * 2) + 1];
+	bu256_hex(hexstr, i);
+
+	json_t *val = json_string(hexstr);
+	assert(val != NULL);
+
+	int rc = json_object_set_new(obj, key, val);
+	assert(rc == 0);
+}
+
+static void bp_json_set_new_script(json_t *obj_parent, const char *key,
+				   GString *s)
+{
+	json_t *obj = json_object();
+	assert(obj != NULL);
+
+	int rc = json_object_set_new(obj_parent, key, obj);
+	assert(rc == 0);
+
+	char raw_hexstr[(s->len * 2) + 1];
+	encode_hex(raw_hexstr, s->str, s->len);
+
+	json_t *hexstr = json_string(raw_hexstr);
+	assert(hexstr != NULL);
+
+	rc = json_object_set_new(obj, "hex", hexstr);
+	assert(rc == 0);
+}
+
+static void output_json_txid(json_t *obj)
+{
+	bp_tx_calc_sha256(&tx);
+
+	bp_json_set_new_int256(obj, "txid", &tx.sha256);
+}
+
+static void output_json_txin(json_t *arr, const struct bp_txin *txin)
+{
+	json_t *obj = json_object();
+	assert(obj != NULL);
+
+	bp_json_set_new_int256(obj, "txid", &txin->prevout.hash);
+	bp_json_set_new_int(obj, "vout", txin->prevout.n);
+	bp_json_set_new_script(obj, "scriptSig", txin->scriptSig);
+	bp_json_set_new_int(obj, "sequence", txin->nSequence);
+
+	json_array_append_new(arr, obj);
+}
+
+static void output_json_vin(json_t *obj)
+{
+	json_t *arr = json_array();
+	assert(arr != NULL);
+
+	int rc = json_object_set_new(obj, "vin", arr);
+	assert(rc == 0);
+
+	if (!tx.vin)
+		return;
+
+	unsigned int i;
+	for (i = 0; i < tx.vin->len; i++) {
+		struct bp_txin *txin = g_ptr_array_index(tx.vin, i);
+		output_json_txin(arr, txin);
+	}
+}
+
+static void output_json_txout(json_t *arr, const struct bp_txout *txout,
+				unsigned int n_idx)
+{
+	json_t *obj = json_object();
+	assert(obj != NULL);
+
+	char raw_valstr[VALSTR_SZ];
+	btc_decimal(raw_valstr, VALSTR_SZ, txout->nValue);
+
+	double raw_val = strtod(raw_valstr, NULL);
+	json_t *val = json_real(raw_val);
+	assert(val != NULL);
+
+	int rc = json_object_set_new(obj, "value_FIXME", val);
+	assert(rc == 0);
+
+	bp_json_set_new_int(obj, "n", n_idx);
+	bp_json_set_new_script(obj, "scriptPubKey", txout->scriptPubKey);
+
+	json_array_append_new(arr, obj);
+}
+
+static void output_json_vout(json_t *obj)
+{
+	json_t *arr = json_array();
+	assert(arr != NULL);
+
+	int rc = json_object_set_new(obj, "vout", arr);
+	assert(rc == 0);
+
+	if (!tx.vout)
+		return;
+
+	unsigned int i;
+	for (i = 0; i < tx.vout->len; i++) {
+		struct bp_txout *txout = g_ptr_array_index(tx.vout, i);
+		output_json_txout(arr, txout, i);
+	}
+}
+
+static void output_data_json(void)
+{
+	json_t *obj = json_object();
+	assert(obj != NULL);
+
+	output_json_txid(obj);
+	bp_json_set_new_int(obj, "version", tx.nVersion);
+	bp_json_set_new_int(obj, "locktime", tx.nLockTime);
+	output_json_vin(obj);
+	output_json_vout(obj);
+
+	int rc = json_dumpf(obj, stdout, JSON_INDENT(4) | JSON_SORT_KEYS);
+	assert(rc == 0);
+
+	printf("\n");
+
+	if (opt_strict_free)
+		json_decref(obj);
+}
+
+static void output_data_hex(void)
 {
 	size_t alloc_len = opt_hexdata ? strlen(opt_hexdata) : 512;
 	GString *s = g_string_sized_new(alloc_len);
@@ -353,6 +508,14 @@ static void write_data(void)
 	printf("%s\n", hexstr);
 
 	g_string_free(s, TRUE);
+}
+
+static void output_data(void)
+{
+	if (opt_decode_json)
+		output_data_json();
+	else
+		output_data_hex();
 }
 
 int main (int argc, char *argv[])
@@ -369,8 +532,10 @@ int main (int argc, char *argv[])
 
 	if (!opt_blank)
 		read_data();
+
 	apply_mutations();
-	write_data();
+
+	output_data();
 
 	bp_tx_free(&tx);
 
