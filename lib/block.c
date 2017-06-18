libbitc/libbitc

tests: Added several tx tests Copied tx tests from bitcoin core 0.12 and made some fixes so all tests pass

lib/block.c
@@ -39,30 +39,17 @@ bool bitc_tx_valid(const struct bitc_tx *tx)
 {
 	unsigned int i;
 
+	// Basic checks
 	if (!tx->vin || !tx->vin->len)
 		return false;
 	if (!tx->vout || !tx->vout->len)
 		return false;
 
+	// Size limits
 	if (bitc_tx_ser_size(tx) > MAX_BLOCK_SIZE)
 		return false;
 
-	if (bitc_tx_coinbase(tx)) {
-		struct bitc_txin *txin = parr_idx(tx->vin, 0);
-
-		if (txin->scriptSig->len < 2 ||
-		    txin->scriptSig->len > 100)
-			return false;
-	} else {
-		for (i = 0; i < tx->vin->len; i++) {
-			struct bitc_txin *txin;
-
-			txin = parr_idx(tx->vin, i);
-			if (!bitc_txin_valid(txin))
-				return false;
-		}
-	}
-
+	// Check for negative or overflow output values
 	int64_t value_total = 0;
 	for (i = 0; i < tx->vout->len; i++) {
 		struct bitc_txout *txout;
@@ -77,8 +64,25 @@ bool bitc_tx_valid(const struct bitc_tx *tx)
 	if (!bitc_valid_value(value_total))
 		return false;
 
+	// Check for duplicate inputs
 	if (bitc_has_dup_inputs(tx))
 		return false;
+
+	if (bitc_tx_coinbase(tx)) {
+		struct bitc_txin *txin = parr_idx(tx->vin, 0);
+
+		if (txin->scriptSig->len < 2 ||
+		    txin->scriptSig->len > 100)
+			return false;
+	} else {
+		for (i = 0; i < tx->vin->len; i++) {
+			struct bitc_txin *txin;
+
+			txin = parr_idx(tx->vin, i);
+			if (bitc_outpt_null(&txin->prevout))
+				return false;
+		}
+	}
 
 	return true;
 }

lib/script_eval.c
@@ -118,9 +118,9 @@ void bitc_tx_sigserializer(cstring *s, const cstring *scriptCode,
 		struct bitc_txout *txout = parr_idx(txTo->vout, nOutput);
 		if (fHashSingle && (nOutput != nIn))
 			// Do not lock-in the txout payee at other indices as txin;
-			ser_s64(s, (int)-1);
-		else
-			ser_bitc_txout(s, txout);
+			bitc_txout_set_null(txout);
+
+		ser_bitc_txout(s, txout);
     }
     // Serialize nLockTime
     ser_u32(s, txTo->nLockTime);
@@ -1388,6 +1388,7 @@ static bool bitc_script_eval(parr *stack, const cstring *script,
 			goto out;
 		}
 
+		// Size limits
 		if (stack->len + altstack->len > 1000)
 			goto out;
 	}

test/tx-valid.c
@@ -78,6 +78,7 @@ static void test_tx_valid(bool is_valid, struct bitc_hashtab *input_map,
 
 	bitc_tx_calc_sha256(&tx);
 
+	bool state = true;
 	unsigned int i;
 	for (i = 0; i < tx.vin->len; i++) {
 		struct bitc_txin *txin;
@@ -110,6 +111,9 @@ static void test_tx_valid(bool is_valid, struct bitc_hashtab *input_map,
 		bool rc = bitc_script_verify(txin->scriptSig, scriptPubKey,
 					&tx, i,
 					test_flags, 0);
+
+		state &= rc;
+
 		if (rc != is_valid) {
 			char tx_hexstr[BU256_STRSZ];
 			bu256_hex(tx_hexstr, &tx.sha256);
@@ -118,10 +122,9 @@ static void test_tx_valid(bool is_valid, struct bitc_hashtab *input_map,
 			"tx-valid: TX %s\n"
 			"tx-valid: txin %u script verification failed\n",
 				tx_hexstr, i);
-
-			assert(rc == is_valid);
 		}
 	}
+	assert(state == is_valid);
 
 out:
 	bitc_tx_free(&tx);
@@ -144,82 +147,101 @@ static void runtest(bool is_valid, const char *json_base_fn)
 	for (idx = 0; idx < cJSON_GetArraySize(tests); idx++) {
 		cJSON *test = cJSON_GetArrayItem(tests, idx);
 
+		assert((test->type & 0xFF) == cJSON_Array);
 		if ((cJSON_GetArrayItem(test, 0)->type & 0xFF) != cJSON_Array) {
 			const char *cmt =
 				cJSON_GetArrayItem(test, 0)->valuestring;
 			if (cmt)
-				parr_add(comments, strdup(cmt));
-			continue;			/* comments */
+				parr_add(comments, strdup(cmt)); /* comments */
+		} else {
+		    assert(cJSON_GetArraySize(test) == 3);
+		    assert((cJSON_GetArrayItem(test, 1)->type & 0xFF) == cJSON_String);
+		    assert((cJSON_GetArrayItem(test, 2)->type & 0xFF) == cJSON_String);
+
+		    cJSON *inputs = cJSON_GetArrayItem(test, 0);
+		    assert((inputs->type & 0xFF) == cJSON_Array);
+		    static unsigned int verify_flags;
+
+		    bitc_hashtab_clear(input_map);
+
+		    unsigned int i;
+		    for (i = 0; i < cJSON_GetArraySize(inputs); i++) {
+				cJSON *input = cJSON_GetArrayItem(inputs, i);
+				assert((input->type & 0xFF) == cJSON_Array);
+
+				const char *prev_hashstr =
+					cJSON_GetArrayItem(input, 0)->valuestring;
+				int prev_n =
+					cJSON_GetArrayItem(input, 1)->valueint;
+				const char *prev_pubkey_enc =
+					cJSON_GetArrayItem(input, 2)->valuestring;
+
+				assert(prev_hashstr != NULL);
+				assert(cJSON_GetArrayItem(input, 1)->type == cJSON_Number);
+				assert(prev_pubkey_enc != NULL);
+
+				struct bitc_outpt *outpt;
+				outpt = malloc(sizeof(*outpt));
+				hex_bu256(&outpt->hash, prev_hashstr);
+				outpt->n = prev_n;
+
+				cstring *script = parse_script_str(prev_pubkey_enc);
+				assert(script != NULL);
+
+				bitc_hashtab_put(input_map, outpt, script);
+		    }
+
+		    const char *tx_hexser =
+					cJSON_GetArrayItem(test, 1)->valuestring;
+		    assert(tx_hexser != NULL);
+
+		    verify_flags = SCRIPT_VERIFY_NONE;
+
+		    const char *json_flags = cJSON_GetArrayItem(test, 2)->valuestring;
+
+		    if (strlen(json_flags) > 0) {
+				const char* json_flag  = strtok((char *)json_flags, ",");
+
+				do {
+					if (strcmp(json_flag, "P2SH") == 0)
+					    verify_flags |= SCRIPT_VERIFY_P2SH;
+					else if (strcmp(json_flag, "STRICTENC") == 0)
+					    verify_flags |= SCRIPT_VERIFY_STRICTENC;
+					else if (strcmp(json_flag, "DERSIG") == 0)
+					    verify_flags |= SCRIPT_VERIFY_DERSIG;
+					else if (strcmp(json_flag, "LOW_S") == 0)
+					    verify_flags |= SCRIPT_VERIFY_LOW_S;
+					else if (strcmp(json_flag, "NULLDUMMY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_NULLDUMMY;
+					else if (strcmp(json_flag, "SIGPUSHONLY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_SIGPUSHONLY;
+					else if (strcmp(json_flag, "MINIMALDATA") == 0)
+					    verify_flags |= SCRIPT_VERIFY_MINIMALDATA;
+					else if (strcmp(json_flag, "DISCOURAGE_UPGRADABLE_NOPS") == 0)
+					    verify_flags |= SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS;
+					else if (strcmp(json_flag, "CLEANSTACK") == 0)
+					    verify_flags |= SCRIPT_VERIFY_CLEANSTACK;
+					else if (strcmp(json_flag, "CHECKLOCKTIMEVERIFY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY;
+					else if (strcmp(json_flag, "CHECKSEQUENCEVERIFY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_CHECKSEQUENCEVERIFY;
+					json_flag = strtok(NULL, ",");
+				} while (json_flag);
+		    }
+
+		    cstring *tx_ser = hex2str(tx_hexser);
+		    assert(tx_ser != NULL);
+
+		    test_tx_valid(is_valid, input_map, tx_ser, verify_flags);
+
+		    cstr_free(tx_ser, true);
+
+		    if (comments->len > 0) {
+				parr_free(comments, true);
+				comments = parr_new(8, free);
+		    }
 		}
-
-		assert((test->type & 0xFF) == cJSON_Array);
-		assert(cJSON_GetArraySize(test) == 3);
-		assert((cJSON_GetArrayItem(test, 1)->type & 0xFF) == cJSON_String);
-		assert((cJSON_GetArrayItem(test, 2)->type & 0xFF) == cJSON_String);
-
-		cJSON *inputs = cJSON_GetArrayItem(test, 0);
-		assert((inputs->type & 0xFF) == cJSON_Array);
-		static unsigned int verify_flags;
-
-		bitc_hashtab_clear(input_map);
-
-		unsigned int i;
-		for (i = 0; i < cJSON_GetArraySize(inputs); i++) {
-			cJSON *input = cJSON_GetArrayItem(inputs, i);
-			assert((input->type & 0xFF) == cJSON_Array);
-
-			const char *prev_hashstr =
-				cJSON_GetArrayItem(input, 0)->valuestring;
-			int prev_n =
-				cJSON_GetArrayItem(input, 1)->valueint;
-			const char *prev_pubkey_enc =
-				cJSON_GetArrayItem(input, 2)->valuestring;
-
-			assert(prev_hashstr != NULL);
-			assert(cJSON_GetArrayItem(input, 1)->type == cJSON_Number);
-			assert(prev_pubkey_enc != NULL);
-
-			struct bitc_outpt *outpt;
-			outpt = malloc(sizeof(*outpt));
-			hex_bu256(&outpt->hash, prev_hashstr);
-			outpt->n = prev_n;
-
-			cstring *script = parse_script_str(prev_pubkey_enc);
-			assert(script != NULL);
-
-			bitc_hashtab_put(input_map, outpt, script);
-		}
-
-		const char *tx_hexser =
-			cJSON_GetArrayItem(test, 1)->valuestring;
-		assert(tx_hexser != NULL);
-
-		verify_flags = SCRIPT_VERIFY_NONE;
-
-		const char *json_flags = cJSON_GetArrayItem(test, 2)->valuestring;
-
-		if (strlen(json_flags) > 0) {
-			const char* json_flag  = strtok((char *)json_flags, ",");
-
-			do {
-				if (strcmp(json_flag, "P2SH") == 0)
-					verify_flags |= SCRIPT_VERIFY_P2SH;
-				json_flag = strtok(NULL, ",");
-			} while (json_flag);
-		}
-
-		cstring *tx_ser = hex2str(tx_hexser);
-		assert(tx_ser != NULL);
-
-		test_tx_valid(is_valid, input_map, tx_ser, verify_flags);
-
-		cstr_free(tx_ser, true);
-
-		if (comments->len > 0) {
-			parr_free(comments, true);
-			comments = parr_new(8, free);
-		}
-	}
+    }
 
 	parr_free(comments, true);
 	comments = NULL;

jgarzik/picocoin

lib/block.c
@@ -39,30 +39,17 @@ bool bp_tx_valid(const struct bp_tx *tx)
 {
 	unsigned int i;
 
+	// Basic checks
 	if (!tx->vin || !tx->vin->len)
 		return false;
 	if (!tx->vout || !tx->vout->len)
 		return false;
 
+	// Size limits
 	if (bp_tx_ser_size(tx) > MAX_BLOCK_SIZE)
 		return false;
 
-	if (bp_tx_coinbase(tx)) {
-		struct bp_txin *txin = parr_idx(tx->vin, 0);
-
-		if (txin->scriptSig->len < 2 ||
-		    txin->scriptSig->len > 100)
-			return false;
-	} else {
-		for (i = 0; i < tx->vin->len; i++) {
-			struct bp_txin *txin;
-
-			txin = parr_idx(tx->vin, i);
-			if (!bp_txin_valid(txin))
-				return false;
-		}
-	}
-
+	// Check for negative or overflow output values
 	int64_t value_total = 0;
 	for (i = 0; i < tx->vout->len; i++) {
 		struct bp_txout *txout;
@@ -77,8 +64,25 @@ bool bp_tx_valid(const struct bp_tx *tx)
 	if (!bp_valid_value(value_total))
 		return false;
 
+	// Check for duplicate inputs
 	if (bp_has_dup_inputs(tx))
 		return false;
+
+	if (bp_tx_coinbase(tx)) {
+		struct bp_txin *txin = parr_idx(tx->vin, 0);
+
+		if (txin->scriptSig->len < 2 ||
+		    txin->scriptSig->len > 100)
+			return false;
+	} else {
+		for (i = 0; i < tx->vin->len; i++) {
+			struct bp_txin *txin;
+
+			txin = parr_idx(tx->vin, i);
+			if (bp_outpt_null(&txin->prevout))
+				return false;
+		}
+	}
 
 	return true;
 }

lib/script_eval.c
@@ -118,9 +118,9 @@ void bp_tx_sigserializer(cstring *s, const cstring *scriptCode,
 		struct bp_txout *txout = parr_idx(txTo->vout, nOutput);
 		if (fHashSingle && (nOutput != nIn))
 			// Do not lock-in the txout payee at other indices as txin;
-			ser_s64(s, (int)-1);
-		else
-			ser_bp_txout(s, txout);
+			bp_txout_set_null(txout);
+
+		ser_bp_txout(s, txout);
     }
     // Serialize nLockTime
     ser_u32(s, txTo->nLockTime);
@@ -1388,6 +1388,7 @@ static bool bp_script_eval(parr *stack, const cstring *script,
 			goto out;
 		}
 
+		// Size limits
 		if (stack->len + altstack->len > 1000)
 			goto out;
 	}

test/tx-valid.c
@@ -64,6 +64,7 @@ static void test_tx_valid(bool is_valid, struct bp_hashtab *input_map,
 
 	bp_tx_calc_sha256(&tx);
 
+	bool state = true;
 	unsigned int i;
 	for (i = 0; i < tx.vin->len; i++) {
 		struct bp_txin *txin;
@@ -96,6 +97,9 @@ static void test_tx_valid(bool is_valid, struct bp_hashtab *input_map,
 		bool rc = bp_script_verify(txin->scriptSig, scriptPubKey,
 					&tx, i,
 					test_flags, 0);
+
+		state &= rc;
+
 		if (rc != is_valid) {
 			char tx_hexstr[BU256_STRSZ];
 			bu256_hex(tx_hexstr, &tx.sha256);
@@ -104,10 +108,9 @@ static void test_tx_valid(bool is_valid, struct bp_hashtab *input_map,
 			"tx-valid: TX %s\n"
 			"tx-valid: txin %u script verification failed\n",
 				tx_hexstr, i);
-
-			assert(rc == is_valid);
 		}
 	}
+	assert(state == is_valid);
 
 out:
 	bp_tx_free(&tx);
@@ -117,6 +120,7 @@ static void runtest(bool is_valid, const char *basefn)
 {
 	char *fn = test_filename(basefn);
 	json_t *tests = read_json(fn);
+	assert(tests != NULL);
 	assert(json_is_array(tests));
 
 	struct bp_hashtab *input_map = bp_hashtab_new_ext(
@@ -129,82 +133,101 @@ static void runtest(bool is_valid, const char *basefn)
 	for (idx = 0; idx < json_array_size(tests); idx++) {
 		json_t *test = json_array_get(tests, idx);
 
+		assert(json_is_array(test));
 		if (!json_is_array(json_array_get(test, 0))) {
 			const char *cmt =
 				json_string_value(json_array_get(test, 0));
 			if (cmt)
-				parr_add(comments, strdup(cmt));
-			continue;			/* comments */
+				parr_add(comments, strdup(cmt)); /* comments */
+		} else {
+		    assert(json_array_size(test) == 3);
+		    assert(json_is_string(json_array_get(test, 1)));
+		    assert(json_is_string(json_array_get(test, 2)));
+
+		    json_t *inputs = json_array_get(test, 0);
+		    assert(json_is_array(inputs));
+		    static unsigned int verify_flags;
+
+		    bp_hashtab_clear(input_map);
+
+		    unsigned int i;
+		    for (i = 0; i < json_array_size(inputs); i++) {
+				json_t *input = json_array_get(inputs, i);
+				assert(json_is_array(input));
+
+				const char *prev_hashstr =
+					json_string_value(json_array_get(input, 0));
+				int prev_n =
+					json_integer_value(json_array_get(input, 1));
+				const char *prev_pubkey_enc =
+					json_string_value(json_array_get(input, 2));
+
+				assert(prev_hashstr != NULL);
+				assert(json_is_integer(json_array_get(input, 1)));
+				assert(prev_pubkey_enc != NULL);
+
+				struct bp_outpt *outpt;
+				outpt = malloc(sizeof(*outpt));
+				hex_bu256(&outpt->hash, prev_hashstr);
+				outpt->n = prev_n;
+
+				cstring *script = parse_script_str(prev_pubkey_enc);
+				assert(script != NULL);
+
+				bp_hashtab_put(input_map, outpt, script);
+		    }
+
+		    const char *tx_hexser =
+					json_string_value(json_array_get(test, 1));
+		    assert(tx_hexser != NULL);
+
+		    verify_flags = SCRIPT_VERIFY_NONE;
+
+		    const char *json_flags = json_string_value(json_array_get(test, 2));
+
+		    if (strlen(json_flags) > 0) {
+				const char* json_flag  = strtok((char *)json_flags, ",");
+
+				do {
+					if (strcmp(json_flag, "P2SH") == 0)
+					    verify_flags |= SCRIPT_VERIFY_P2SH;
+					else if (strcmp(json_flag, "STRICTENC") == 0)
+					    verify_flags |= SCRIPT_VERIFY_STRICTENC;
+					else if (strcmp(json_flag, "DERSIG") == 0)
+					    verify_flags |= SCRIPT_VERIFY_DERSIG;
+					else if (strcmp(json_flag, "LOW_S") == 0)
+					    verify_flags |= SCRIPT_VERIFY_LOW_S;
+					else if (strcmp(json_flag, "NULLDUMMY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_NULLDUMMY;
+					else if (strcmp(json_flag, "SIGPUSHONLY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_SIGPUSHONLY;
+					else if (strcmp(json_flag, "MINIMALDATA") == 0)
+					    verify_flags |= SCRIPT_VERIFY_MINIMALDATA;
+					else if (strcmp(json_flag, "DISCOURAGE_UPGRADABLE_NOPS") == 0)
+					    verify_flags |= SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS;
+					else if (strcmp(json_flag, "CLEANSTACK") == 0)
+					    verify_flags |= SCRIPT_VERIFY_CLEANSTACK;
+					else if (strcmp(json_flag, "CHECKLOCKTIMEVERIFY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY;
+					else if (strcmp(json_flag, "CHECKSEQUENCEVERIFY") == 0)
+					    verify_flags |= SCRIPT_VERIFY_CHECKSEQUENCEVERIFY;
+					json_flag = strtok(NULL, ",");
+				} while (json_flag);
+		    }
+
+		    cstring *tx_ser = hex2str(tx_hexser);
+		    assert(tx_ser != NULL);
+
+		    test_tx_valid(is_valid, input_map, tx_ser, verify_flags);
+
+		    cstr_free(tx_ser, true);
+
+		    if (comments->len > 0) {
+				parr_free(comments, true);
+				comments = parr_new(8, free);
+		    }
 		}
-
-		assert(json_is_array(test));
-		assert(json_array_size(test) == 3);
-		assert(json_is_string(json_array_get(test, 1)));
-		assert(json_is_string(json_array_get(test, 2)));
-
-		json_t *inputs = json_array_get(test, 0);
-		assert(json_is_array(inputs));
-		static unsigned int verify_flags;
-
-		bp_hashtab_clear(input_map);
-
-		unsigned int i;
-		for (i = 0; i < json_array_size(inputs); i++) {
-			json_t *input = json_array_get(inputs, i);
-			assert(json_is_array(input));
-
-			const char *prev_hashstr =
-				json_string_value(json_array_get(input, 0));
-			int prev_n =
-				json_integer_value(json_array_get(input, 1));
-			const char *prev_pubkey_enc =
-				json_string_value(json_array_get(input, 2));
-
-			assert(prev_hashstr != NULL);
-			assert(json_is_integer(json_array_get(input, 1)));
-			assert(prev_pubkey_enc != NULL);
-
-			struct bp_outpt *outpt;
-			outpt = malloc(sizeof(*outpt));
-			hex_bu256(&outpt->hash, prev_hashstr);
-			outpt->n = prev_n;
-
-			cstring *script = parse_script_str(prev_pubkey_enc);
-			assert(script != NULL);
-
-			bp_hashtab_put(input_map, outpt, script);
-		}
-
-		const char *tx_hexser =
-			json_string_value(json_array_get(test, 1));
-		assert(tx_hexser != NULL);
-
-		verify_flags = SCRIPT_VERIFY_NONE;
-
-		const char *json_flags = json_string_value(json_array_get(test, 2));
-
-		if (strlen(json_flags) > 0) {
-			const char* json_flag  = strtok((char *)json_flags, ",");
-
-			do {
-				if (strcmp(json_flag, "P2SH") == 0)
-					verify_flags |= SCRIPT_VERIFY_P2SH;
-				json_flag = strtok(NULL, ",");
-			} while (json_flag);
-		}
-
-		cstring *tx_ser = hex2str(tx_hexser);
-		assert(tx_ser != NULL);
-
-		test_tx_valid(is_valid, input_map, tx_ser, verify_flags);
-
-		cstr_free(tx_ser, true);
-
-		if (comments->len > 0) {
-			parr_free(comments, true);
-			comments = parr_new(8, free);
-		}
-	}
+    }
 
 	parr_free(comments, true);
 	comments = NULL;